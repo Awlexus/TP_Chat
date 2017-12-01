031f8150cf41f6c195f45d330113a471c1d5082d
Awlex <wieseralex580@gmail.com>
2017-12-01 18:27:09 +0100

    more exports

--- src/gui/TextChangedEvent.kt
+++ src/gui/TextChangedEvent.kt
@@ -3,7 +3,8 @@ package gui
 import javax.swing.JTextField
 
 /**
- * Created by Awlex on 01.12.2017.
+ * @author Matteo Cosi
+ * @since 01.12.2017
  */
 class TextChangedEvent(val source: JTextField) {
 
