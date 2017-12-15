e46d1a275bda77dfefd7b43f00070ed99e1e9053
Awlex <wieseralex580@gmail.com>
2017-12-15 09:25:46 +0100

    Imporved outputs

--- src/tests/ChatTest.kt
+++ src/tests/ChatTest.kt
@@ -1,12 +1,60 @@
 package tests
 
 import protocol.Protocol
+import protocol.ProtocolCallback
+import java.net.DatagramPacket
+import java.net.InetAddress
 
 /**
  * Created by Awlex on 15.12.2017.
  */
 fun main(args: Array<String>) {
-    val prot = Protocol("Matteo")
+    val set = HashMap<InetAddress, String>()
+    val prot = Protocol("Matteo", object : ProtocolCallback {
+        override fun hello(packet: DatagramPacket, username: String) {
+            set[packet.address] = username
+
+            println("$username joined")
+        }
+
+        override fun world(packet: DatagramPacket, username: String) {
+            set[packet.address] = username
+
+            println("$username joined")
+        }
+
+        override fun goodbye(packet: DatagramPacket) {
+            println("${set[packet.address]} left")
+
+            set.remove(packet.address)
+
+        }
+
+        override fun typing(packet: DatagramPacket, typing: Boolean) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun message(packet: DatagramPacket, message: String) {
+            println("${set[packet.address]}: $message")
+        }
+
+        override fun existsGroupWithId(packet: DatagramPacket, id: Int): Boolean {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun createGroup(packet: DatagramPacket, id: Int, members: Array<InetAddress>) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun denyGroup(packet: DatagramPacket) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun groupMessage(packet: DatagramPacket, groupId: Int, message: String) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    })
     prot.hello()
     do {
         val line = readLine()!!
