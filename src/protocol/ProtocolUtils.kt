85495c64d5616efec71b679a61bfe93ea36d75e5
Awlex <wieseralex580@gmail.com>
2017-12-22 09:03:34 +0100

    Fixed retrieving Mac-address

--- src/protocol/ProtocolUtils.kt
+++ src/protocol/ProtocolUtils.kt
@@ -2,16 +2,17 @@ package protocol
 
 import java.net.DatagramPacket
 import java.net.InetAddress
-import java.net.NetworkInterface
+import java.util.*
 
-fun getMacAddress(packet: DatagramPacket) = getMacAddress(packet.address)
+fun getMacAddress(packet: DatagramPacket) = getMacAdress(packet.address)
 
-fun getMacAddress(ip: InetAddress): String {
-    val iface = NetworkInterface.getByInetAddress(ip)
-    val mac = iface.hardwareAddress
-    return String(mac)
+fun getMacAdress(ip: InetAddress): String? {
+    Runtime.getRuntime().exec("arp -a")
+    Scanner(Runtime.getRuntime().exec("arp -a ${ip.hostAddress}").inputStream).useDelimiter("\\A").use {
+        val reg = Regex("([0-9a-fA-F]{2}-?){6}")
+        return reg.find(it.next())?.value
+    }
 }
-
 fun DatagramPacket.getTextData() = String(this.data, this.offset, this.length)
 
 fun DatagramPacket.getMessage() = getTextData().split(Regex("\\s+"), 2)[1]
